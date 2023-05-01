f54cfcf2125f1a52f08b696cbfd03213b6512b06
Kaan <kenkaraa@gmail.com>
2023-05-01 18:03:00 -0400

    Adds toggleMenu items

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
-import { useState } from "react";
+import { useState, useCallback } from "react";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
-  const [isOpen, setIsOpen] = useStatetate(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
@@ -54,6 +55,30 @@ const UserMenu = () => {
           </div>
         </div>
       </div>
+
+      {isOpen && (
+        <div
+          className="
+            absolute 
+            rounded-xl 
+            shadow-md
+            w-[40vw]
+            md:w-3/4 
+            bg-white 
+            overflow-hidden 
+            right-0 
+            top-12 
+            text-sm
+          "
+        >
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem label="Login" onClick={() => {}} />
+              <MenuItem label="Sign Up" onClick={() => {}} />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
