f5ba94e35903d126647a33385bdd0a6a8bb2168e
Kaan <kenkaraa@gmail.com>
2023-05-02 16:25:30 -0400

    Adds RegisterModal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import Modal from '@/app/components/modals/Modal';
-
+import RegisterModal from "./components/modals/RegisterModal";
 
 const font = Nunito({
   subsets: ["latin"],
@@ -23,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal actionLabel="Submit" title="Testing out!" isOpen/>
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
