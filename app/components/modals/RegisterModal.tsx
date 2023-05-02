5b450a7338d4b2dc7da459c2d97b2db719caa6d0
Kaan <kenkaraa@gmail.com>
2023-05-02 16:59:54 -0400

    Adds Register Form

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -52,6 +52,23 @@ const RegisterModal = () => {
         errors={errors}
         required
       />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
     </div>
   );
 
