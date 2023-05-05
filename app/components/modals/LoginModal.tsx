432432ccd7ce4455e5f50ad860726bbd1d6f3956
Kaan <kenkaraa@gmail.com>
2023-05-05 18:34:26 -0400

    Adds Login functionality

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,67 +1,83 @@
-"use client";
-import axios from "axios";
-import { AiFillGithub } from "react-icons/ai";
-import { FcGoogle } from "react-icons/fc";
+'use client';
+
 import { useCallback, useState } from "react";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { signIn } from 'next-auth/react';
+import { 
+  FieldValues, 
+  SubmitHandler, 
+  useForm
+} from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
+import { AiFillGithub } from "react-icons/ai";
+import { useRouter } from "next/navigation";
+
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
+
 import Modal from "./Modal";
-import Heading from "../Heading";
 import Input from "../inputs/Input";
-import { toast } from "react-hot-toast";
+import Heading from "../Heading";
 import Button from "../Button";
 
 const LoginModal = () => {
-  const registerModal = useRegisterModal();
+  const router = useRouter();
   const loginModal = useLoginModal();
-
+  const registerModal = useRegisterModal();
   const [isLoading, setIsLoading] = useState(false);
 
-  const {
-    register,
+  const { 
+    register, 
     handleSubmit,
-    formState: { errors },
+    formState: {
+      errors,
+    },
   } = useForm<FieldValues>({
     defaultValues: {
-      name: "",
-      email: "",
-      password: "",
+      email: '',
+      password: ''
     },
   });
-
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+  
+  const onSubmit: SubmitHandler<FieldValues> = 
+  (data) => {
     setIsLoading(true);
 
-    axios
-      .post("/api/register", data)
-      .then(() => {
-        registerModal.onClose();
-      })
-      .catch((error) => {
-        toast.error("Something went wrong.");
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  };
+    signIn('credentials', { 
+      ...data, 
+      redirect: false,
+    })
+    .then((callback) => {
+      setIsLoading(false);
+
+      if (callback?.ok) {
+        toast.success('Logged in');
+        router.refresh();
+        loginModal.onClose();
+      }
+      
+      if (callback?.error) {
+        toast.error(callback.error);
+      }
+    });
+  }
+
+  const onToggle = useCallback(() => {
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [loginModal, registerModal])
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Heading title="Welcome to Airbnb" subtitle="Create an account!" />
+      <Heading
+        title="Welcome back"
+        subtitle="Login to your account!"
+      />
       <Input
         id="email"
         label="Email"
         disabled={isLoading}
-        register={register}
-        errors={errors}
-        required
-      />
-      <Input
-        id="name"
-        label="Name"
-        disabled={isLoading}
-        register={register}
+        register={register}  
         errors={errors}
         required
       />
@@ -75,54 +91,44 @@ const LoginModal = () => {
         required
       />
     </div>
-  );
+  )
 
   const footerContent = (
     <div className="flex flex-col gap-4 mt-3">
       <hr />
-      <Button
-        outline
+      <Button 
+        outline 
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => {}}
+        onClick={() => signIn('google')}
       />
-      <Button
-        outline
+      <Button 
+        outline 
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn('github')}
       />
-      <div
-        className="
-          text-neutral-500 
-          text-center 
-          mt-4 
-          font-light
-        "
-      >
-        <p>
-          Already have an account?
-          <span
-            onClick={registerModal.onClose}
+      <div className="
+      text-neutral-500 text-center mt-4 font-light">
+        <p>First time using Airbnb?
+          <span 
+            onClick={onToggle} 
             className="
               text-neutral-800
               cursor-pointer 
               hover:underline
             "
-          >
-            {" "}
-            Log in
-          </span>
+            > Create an account</span>
         </p>
       </div>
     </div>
-  );
+  )
 
   return (
     <Modal
       disabled={isLoading}
       isOpen={loginModal.isOpen}
-      title="Register"
+      title="Login"
       actionLabel="Continue"
       onClose={loginModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
@@ -130,6 +136,6 @@ const LoginModal = () => {
       footer={footerContent}
     />
   );
-};
+}
 
 export default LoginModal;
