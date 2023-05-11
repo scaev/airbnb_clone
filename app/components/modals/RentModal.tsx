f82f7af05d0957af746abee1d68db78cd7905778
Kaan <kenkaraa@gmail.com>
2023-05-11 17:01:00 -0400

    Adds completed UI for categories

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -46,6 +46,14 @@ const RentModal = () => {
 
   const category = watch("category");
 
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  };
+
   const onBack = () => {
     setStep((value) => value - 1);
   };
@@ -88,8 +96,8 @@ const RentModal = () => {
         {categories.map((item) => (
           <div key={item.label} className="col-span-1">
             <CategoryInput
-              onClick={() => {}}
-              selected={false}
+              onClick={(category) => setCustomValue("category", category)}
+              selected={category === item.label}
               label={item.label}
               icon={item.icon}
             />
