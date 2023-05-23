feba9b8f91167bc66b93d784338f96899779ddd3
Kaan <kenkaraa@gmail.com>
2023-05-23 14:16:31 -0400

    Adds finished EmptyState

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -1,5 +1,47 @@
-const EmptyState = () => {
-  return <div>Empty</div>;
+"use client";
+
+//useRouter only works in Client Components
+import { useRouter } from "next/navigation";
+
+import Button from "./Button";
+import Heading from "./Heading";
+
+interface EmptyState {
+  title?: string;
+  subtitle?: string;
+  showReset?: boolean;
+}
+
+const EmptyState: React.FC<EmptyState> = ({
+  title = "No exact matches",
+  subtitle = "Try changing or removing some of your filters.",
+  showReset,
+}) => {
+  const router = useRouter();
+  return (
+    <div
+      className="
+    h-[60vh]
+    flex 
+    flex-col 
+    gap-2 
+    justify-center 
+    items-center 
+  "
+    >
+      {" "}
+      <Heading center title={title} subtitle={subtitle} />
+      <div className="w-48 mt-4">
+        {showReset && (
+          <Button
+            outline
+            label="Remove all filters"
+            onClick={() => router.push("/")}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default EmptyState;
