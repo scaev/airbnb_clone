dcbb45d233131da3d1fe2c2a83f32bd6031c6866
Kaan <kenkaraa@gmail.com>
2023-05-23 16:21:45 -0400

    Button Styling

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SafeUser } from "@/app/types";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 interface HeartButtonProps {
   listingId: string;
@@ -11,10 +12,35 @@ const HeartButton: React.FC<HeartButtonProps> = ({
   listingId,
   currentUser,
 }) => {
-  const hasFavorited = false;
+  const hasFavorited = true;
   const toggleFavorite = () => {};
 
-  return <div></div>;
+  return (
+    <div
+      onClick={toggleFavorite}
+      className="
+    relative
+    hover:opacity-80
+    transition
+    cursor-pointer
+  "
+    >
+      {" "}
+      <AiOutlineHeart
+        size={28}
+        className="
+          fill-white
+          absolute
+          -top-[2px]
+          -right-[2px]
+        "
+      />
+      <AiFillHeart
+        size={24}
+        className={hasFavorited ? "fill-rose-500" : "fill-neutral-500/70"}
+      />
+    </div>
+  );
 };
 
 export default HeartButton;
