8fa803fbca26667ab2c8e76be15537abc2a04568
Kaan <kenkaraa@gmail.com>
2023-05-26 18:59:08 -0400

    Adds Delete func. for favorite listingId

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -32,8 +32,40 @@ export default async function POST(
       id: currentUser.id,
     },
     data: {
-      favoriteIds
-    }
+      favoriteIds,
+    },
+  });
+
+  return NextResponse.json(user);
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds = favoriteIds.filter((id) => id !== listingId);
+
+  const user = await prisma.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: {
+      favoriteIds,
+    },
   });
 
   return NextResponse.json(user);
