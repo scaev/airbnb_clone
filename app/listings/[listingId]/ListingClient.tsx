41a034b18ad082f5db9e1981bdb2ae0b0064b9b7
Kaan <kenkaraa@gmail.com>
2023-05-29 16:00:13 -0400

    Adds ListingHead component

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,5 +1,66 @@
-const ListingClient = () => {
-  return <div>Listing Client</div>;
+"use client";
+
+import Container from "@/app/components/Container";
+import ListingHead from "@/app/components/listings/ListingHead";
+import { categories } from "@/app/components/navbar/Categories";
+import { SafeListing, SafeUser } from "@/app/types";
+import { Reservation } from "@prisma/client";
+import { useMemo } from "react";
+
+interface ListingClientProps {
+  reservations?: Reservation[];
+  listing: SafeListing & {
+    user: SafeUser;
+  };
+  currentUser?: SafeUser | null;
+}
+
+const ListingClient: React.FC<ListingClientProps> = ({
+  listing,
+  currentUser,
+}) => {
+  const category = useMemo(() => {
+    return categories.find((items) => items.label === listing.category);
+  }, [listing.category]);
+
+  return (
+    <Container>
+      <div
+        className="
+      max-w-screen-lg 
+      mx-auto
+    "
+      >
+        <div className="flex flex-col gap-6">
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+          <div
+            className="
+          grid 
+          grid-cols-1 
+          md:grid-cols-7 
+          md:gap-10 
+          mt-6
+        "
+          >
+            <div
+              className="
+            order-first 
+            mb-10 
+            md:order-last 
+            md:col-span-3
+          "
+            ></div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default ListingClient;
