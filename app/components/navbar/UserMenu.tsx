ed5e385cf295bfc7fddb8d83d23c869f870d8f35
Kaan <kenkaraa@gmail.com>
2023-06-04 17:16:45 -0400

    Adds route for properties

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -107,7 +107,10 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                   label="My reservations"
                   onClick={() => router.push("/reservations")}
                 />
-                <MenuItem label="My properties" onClick={() => {}} />
+                <MenuItem
+                  label="My properties"
+                  onClick={() => router.push("/properties")}
+                />
                 <MenuItem label="Airbnb my home" onClick={rentModal.onOpen} />
                 <hr />
                 <MenuItem label="Logout" onClick={() => signOut()} />
