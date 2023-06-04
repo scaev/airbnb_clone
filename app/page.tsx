6a5b229d5ae6066f5cf4f5c604ed1d12880bbea0
Kaan <kenkaraa@gmail.com>
2023-06-04 17:27:40 -0400

    Adds properties page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import getCurrentUser from "./actions/getCurrentUser";
-import getListings from "./actions/getListings";
+import getListings, { IListingsParams } from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
-export default async function Home() {
-  const listings = await getListings();
+interface HomeProps {
+  searchParams: IListingsParams;
+}
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
@@ -35,7 +39,7 @@ export default async function Home() {
         >
           {listings.map((listing) => (
             <ListingCard
-            currentUser={currentUser}
+              currentUser={currentUser}
               key={listing.id}
               data={listing}
             />
@@ -44,4 +48,5 @@ export default async function Home() {
       </Container>
     </ClientOnly>
   );
-}
+};
+export default Home;
