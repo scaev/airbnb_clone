1d3be5a550e7456a5a2da18723e61e22f2670144
Kaan <kenkaraa@gmail.com>
2023-06-04 17:10:40 -0400

    Adds getFavoriteListings action

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -37,6 +37,7 @@ const RegisterModal = () => {
     axios
       .post("/api/register", data)
       .then(() => {
+        toast.success("Successfully Registered.");
         registerModal.onClose();
         loginModal.onOpen();
       })
